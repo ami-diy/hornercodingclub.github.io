4c654cf4ebc8355a37eee940898ca12fe5529c0e
ami-diy <65624679+ami-diy@users.noreply.github.com>
2020-05-19 12:45:01 -0700

    Add files via upload

---
+++ Trinket Download-Hangman/main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# Hangman game
+
+import random
+
+class HangMan(object):
+    
+    # Hangman game
+    hang = []
+    hang.append(' +---+')
+    hang.append(' |   |')
+    hang.append('     |')
+    hang.append('     |')
+    hang.append('     |')
+    hang.append('     |')
+    hang.append('=======')
+    
+    man = {}
+    man[0] = [' 0   |']
+    man[1] = [' 0   |', ' |   |']
+    man[2] = [' 0   |', '/|   |']
+    man[3] = [' 0   |', '/|\\  |']
+    man[4] = [' 0   |', '/|\\  |', '/    |']
+    man[5] = [' 0   |', '/|\\  |', '/ \\  |']
+    
+    pics = []
+    
+    words = '''alligator  aardvark badger beaver cobra cougar coyote donkey dolphin
+    egret elephant ferret frigatebird gazelle groundhog hartebeest hummingbird impala iguana jerboa jaguar
+    klipspringer kerodon lynx lemur macaw muskox narwhal needlefish opossum osprey pangolin panther quetzal
+    rattlesnake roadrunner sailfish surgeonfish tarantula toucan urchin vulture
+    wallaby warthog xerus yellowjacket yak zebra zebu'''.split()
+
+    infStr='_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\''
+  
+    def __init__(self, *args, **kwargs):
+        i, j = 2, 0
+        self.pics.append(self.hang[:])
+        for ls in self.man.values():
+            pic, j = self.hang[:], 0
+            for m in ls:
+                pic[i + j] = m
+                j += 1
+            self.pics.append(pic)
+
+    def pickWord(self):
+        return self.words[random.randint(0, len(self.words) - 1)]
+    
+    def printPic(self, idx, wordLen):
+        for line in self.pics[idx]:
+            print(line)
+            
+    def askAndEvaluate(self, word, result, missed):
+        guess = input()
+        if guess == None or len(guess) != 1 or (guess in result) or (guess in missed):
+            return None, False
+        i = 0
+        right = guess in word
+        for c in word:
+            if c == guess:
+                result[i] = c
+            i += 1
+        return guess, right
+
+    def info(self, info):
+        ln=len(self.infStr)
+        print(self.infStr[:-3])
+        print(info)
+        print(self.infStr[3:])
+            
+    def start(self):
+        print('Welcome to Hangman! Guess an animal!')
+        word = list(self.pickWord())
+        result = list('*' * len(word))
+        print('The word is: ', result)
+        success, i, missed = False, 0, []
+        while i < len(self.pics)-1:
+            print('Guess a letter: ', end='')
+            guess,right = self.askAndEvaluate(word, result, missed)
+            if guess == None:
+                print('You\'ve already entered this character.')
+                continue
+            print(''.join(result))
+            if result == word:
+                self.info('Congratulations! You won!')
+                success = True
+                break
+            if not right:
+                missed.append(guess)
+                i+=1
+            self.printPic(i, len(word))
+            print('Missed characters: ', missed)
+        
+        if not success:
+            self.info('The word was \''+''.join(word)+'\' ! Try again next time!')
+
+a = HangMan().start()
